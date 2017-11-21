5ffe18d8cf42a90f7e9fd46ceeb8c3bba7be6c6b
Caleb Phillips <calebrphillips@gmail.com>
2017-11-21 01:34:03 +0000

    Dont download data in install script

--- gpublast/setup_gpublast.sh
+++ gpublast/setup_gpublast.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script to install GPUBlast and download data
+# Script to install GPUBlast 
 # Assumes that setup_cuda.sh has already been run
 
 # GPUBlast Url
@@ -8,14 +8,6 @@ URL=http://thales.cheme.cmu.edu/gpublast/gpu-blast-1.1_ncbi-blast-2.2.28.tar.gz
 
 # Archive name
 ARCHIVE=gpu-blast-1.1_ncbi-blast-2.2.28.tar.gz
-
-# Data Urls
-DATA1=ftp://ftp.ncbi.nlm.nih.gov/blast/db/env_nr.00.tar.gz
-DATA2=ftp://ftp.ncbi.nlm.nih.gov/blast/db/env_nr.01.tar.gz
-
-#tarball names
-TARBALL1=env_nr.00.tar.gz
-TARBALL2=env_nr.01.tar.gz
 
 # Download Binaries
 mkdir -p ~/blast
@@ -30,17 +22,8 @@ rm $ARCHIVE
 chmod +x install
 printf 'no\n' | sudo ./install
 
-# Download data
-mkdir -p ~/data/database
-cd ~/data/database
-wget $DATA1
-wget $DATA2
+#Update bashrc
+echo 'export PATH=$PATH:~/blast/ncbi-blast-2.2.28+-src/c++/GCC480-ReleaseMT64/bin' >> ~/.bashrc
 
-tar -xzvf $TARBALL1
-rm $TARBALL1
-tar -xzvf $TARBALL2
-rm $TARBALL2
-
-echo 'Install GPUBlast and downloaded data'
-echo 'Need to download queries'
+echo 'Installed GPUBlast'
 
