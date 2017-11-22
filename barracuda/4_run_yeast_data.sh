e51bdc0986723984bddcfb45ad102334bc410e8f
Caleb Phillips <calebrphillips@gmail.com>
2017-11-22 20:19:11 +0000

    Rename to yeast

--- barracuda/4_run_yeast_data.sh
+++ barracuda/4_run_yeast_data.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-fasta=~/data/human_data/SRR002062.fasta
-fastq=~/data/human_data/SRR002062.fastq
+fasta=~/data/yeast_data/SRR002062.fasta
+fastq=~/data/yeast_data/SRR002062.fastq
 
-mkdir -p ~/data/human_output
+mkdir -p ~/data/yeast_output
 
 barracuda index $fasta
 
-barracuda aln $fasta $fastq > ~/data/human_output/human.sai
+barracuda aln $fasta $fastq > ~/data/yeast_output/yeast.sai
 
-barracuda samse $fasta ~/data/human_output/human.sai $fastq > ~/data/human_output/human.sam
+barracuda samse $fasta ~/data/yeast_output/yeast.sai $fastq > ~/data/yeast_output/yeast.sam
