438185c9cf097808a4a8f3ab9a111edcc5ac95c8
Caleb Phillips <calebrphillips@gmail.com>
2017-11-22 23:50:03 +0000

    Add vtune to profile script

--- gpublast/profile.sh
+++ gpublast/profile.sh
@@ -4,4 +4,6 @@ timestamp=$(date +%Y-%m-%d_%H-%M-%S )
 
 mkdir -p ~/profile-results
 
+amplxe-cl -collect hotspots -r ~/profile-results/gpublast-vtune-$timestamp blastp -query ~/data/queries/SequenceLength_00000100.txt -db ~/data/database/sorted_env_nr -gpu t
+
 nvprof --analysis-metrics -o ~/profile-results/gpublast-nvprof-$timestamp.nvprof blastp -query ~/data/queries/SequenceLength_00000100.txt -db ~/data/database/sorted_env_nr -gpu t
