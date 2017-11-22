02ab20831d936cb4ec79f590d983fe16199afa40
Caleb Phillips <calebrphillips@gmail.com>
2017-11-22 22:17:19 +0000

    Add start of script to profile barracuda

--- barracuda/profile.sh
+++ barracuda/profile.sh
@@ -8,10 +8,11 @@ mkdir -p ~/data/yeast_output
 timestamp=$(date +%Y-%m-%d_%H-%M-%S )
 mkdir -p ~/profile-results
 
-cmd1="barracuda index $fasta"
-cmd2="barracuda aln $fasta $fastq > ~/data/yeast_output/yeast.sai"
-cmd3="barracuda samse $fasta ~/data/yeast_output/yeast.sai $fastq > ~/data/yeast_output/yeast.sam"
+# Run vTune here
+barracuda index $fasta
 
-nvprof --analysis-metrics -o ~/profile-results/barracuda-stage1-nvprof-$timestamp.nvprof $cmd1
-nvprof --analysis-metrics -o ~/profile-results/barracuda-stage2-nvprof-$timestamp.nvprof $cmd2
-nvprof --analysis-metrics -o ~/profile-results/barracuda-stage3-nvprof-$timestamp.nvprof $cmd3
+# Cuda Bound
+nvprof --analysis-metrics -o ~/profile-results/barracuda-stage2-nvprof-$timestamp.nvprof barracuda aln $fasta $fastq > ~/data/yeast_output/yeast.sai
+
+# Run vTune here
+barracuda samse $fasta ~/data/yeast_output/yeast.sai $fastq > ~/data/yeast_output/yeast.sam
