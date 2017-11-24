cf815678e732127b9d1bebd68d267428c57d4cb8
Caleb Phillips <calebrphillips@gmail.com>
2017-11-24 20:54:17 +0000

    Use sample data for baseline profiling (Full takes a long time)

--- barracuda/profile.sh
+++ barracuda/profile.sh
@@ -4,9 +4,9 @@
 echo 0 | sudo dd of=/proc/sys/kernel/yama/ptrace_scope
 source /opt/intel/vtune_amplifier_2018/amplxe-vars.sh
 
-fasta=~/data/yeast_data/SRR002062.fasta
-fastq=~/data/yeast_data/SRR002062.fastq
-mkdir -p ~/data/yeast_output
+fasta=~/data/sample_data/Saccharomyces_cerevisiae.SGD1.01.50.dna_rm.toplevel.fa
+fastq=~/data/sample_data/sample_reads.fastq
+mkdir -p ~/data/sample_output
 
 timestamp=$(date +%Y-%m-%d_%H-%M-%S )
 mkdir -p ~/profile-results
@@ -15,8 +15,8 @@ mkdir -p ~/profile-results
 amplxe-cl -collect hotspots -r ~/profile-results/barracuda-stage1-vtune-$timestamp barracuda index $fasta
 
 # Cuda Bound
-amplxe-cl -collect hotspots -r ~/profile-results/barracuda-stage2-vtune-$timestamp barracuda aln $fasta $fastq > ~/data/yeast_output/yeast.sai
-nvprof --analysis-metrics -o ~/profile-results/barracuda-stage2-nvprof-$timestamp.nvprof barracuda aln $fasta $fastq > ~/data/yeast_output/yeast.sai
+amplxe-cl -collect hotspots -r ~/profile-results/barracuda-stage2-vtune-$timestamp barracuda aln $fasta $fastq > ~/data/sample_output/sample.sai
+nvprof --analysis-metrics -o ~/profile-results/barracuda-stage2-nvprof-$timestamp.nvprof barracuda aln $fasta $fastq > ~/data/sample_output/sample.sai
 
 # Run vTune here
-amplxe-cl -collect hotspots -r ~/profile-results/barracuda-stage3-vtune-$timestamp barracuda samse $fasta ~/data/yeast_output/yeast.sai $fastq > ~/data/yeast_output/yeast.sam
+amplxe-cl -collect hotspots -r ~/profile-results/barracuda-stage3-vtune-$timestamp barracuda samse $fasta ~/data/sample_output/sample.sai $fastq > ~/data/sample_output/sample.sam
---
+++ barracuda/profile_full.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Prereq for vtune
+echo 0 | sudo dd of=/proc/sys/kernel/yama/ptrace_scope
+source /opt/intel/vtune_amplifier_2018/amplxe-vars.sh
+
+fasta=~/data/yeast_data/SRR002062.fasta
+fastq=~/data/yeast_data/SRR002062.fastq
+mkdir -p ~/data/yeast_output
+
+timestamp=$(date +%Y-%m-%d_%H-%M-%S )
+mkdir -p ~/profile-results
+
+# Run vTune here
+amplxe-cl -collect hotspots -r ~/profile-results/barracuda-stage1-vtune-$timestamp barracuda index $fasta
+
+# Cuda Bound
+amplxe-cl -collect hotspots -r ~/profile-results/barracuda-stage2-vtune-$timestamp barracuda aln $fasta $fastq > ~/data/yeast_output/yeast.sai
+nvprof --analysis-metrics -o ~/profile-results/barracuda-stage2-nvprof-$timestamp.nvprof barracuda aln $fasta $fastq > ~/data/yeast_output/yeast.sai
+
+# Run vTune here
+amplxe-cl -collect hotspots -r ~/profile-results/barracuda-stage3-vtune-$timestamp barracuda samse $fasta ~/data/yeast_output/yeast.sai $fastq > ~/data/yeast_output/yeast.sam
