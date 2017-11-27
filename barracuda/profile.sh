c000de74f168b9508c5f0700377570b40f858bc8
Caleb Phillips <calebrphillips@gmail.com>
2017-11-27 20:06:02 +0000

    Profile only nvidia and do n times for non full

--- barracuda/profile.sh
+++ barracuda/profile.sh
@@ -8,15 +8,21 @@ fasta=~/data/sample_data/Saccharomyces_cerevisiae.SGD1.01.50.dna_rm.toplevel.fa
 fastq=~/data/sample_data/sample_reads.fastq
 mkdir -p ~/data/sample_output
 
-timestamp=$(date +%Y-%m-%d_%H-%M-%S )
 mkdir -p ~/profile-results
 
-# Run vTune here
-amplxe-cl -collect hotspots -r ~/profile-results/barracuda-stage1-vtune-$timestamp barracuda index $fasta
+test_count=5
 
-# Cuda Bound
-amplxe-cl -collect hotspots -r ~/profile-results/barracuda-stage2-vtune-$timestamp barracuda aln $fasta $fastq > ~/data/sample_output/sample.sai
-nvprof --analysis-metrics -o ~/profile-results/barracuda-stage2-nvprof-$timestamp.nvprof barracuda aln $fasta $fastq > ~/data/sample_output/sample.sai
+for (( c=1; c<=$test_count; c++ ))
+do
+    timestamp=$(date +%Y-%m-%d_%H-%M-%S )
 
-# Run vTune here
-amplxe-cl -collect hotspots -r ~/profile-results/barracuda-stage3-vtune-$timestamp barracuda samse $fasta ~/data/sample_output/sample.sai $fastq > ~/data/sample_output/sample.sam
+    # Run vTune here
+    amplxe-cl -collect hotspots -r ~/profile-results/barracuda-stage1-vtune-$timestamp barracuda index $fasta
+
+    # Cuda Bound
+    amplxe-cl -collect hotspots -r ~/profile-results/barracuda-stage2-vtune-$timestamp barracuda aln $fasta $fastq > ~/data/sample_output/sample.sai
+    #nvprof --analysis-metrics -o ~/profile-results/barracuda-stage2-nvprof-$timestamp.nvprof barracuda aln $fasta $fastq > ~/data/sample_output/sample.sai
+
+    # Run vTune here
+    amplxe-cl -collect hotspots -r ~/profile-results/barracuda-stage3-vtune-$timestamp barracuda samse $fasta ~/data/sample_output/sample.sai $fastq > ~/data/sample_output/sample.sam
+done
